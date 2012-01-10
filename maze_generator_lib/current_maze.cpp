34c65550c16931afc23078db1f7f6c0a561e9df2
Roman Verchykov <roman.verchikov@gmail.com>
2012-01-10 15:07:34 +0200

    Removed unneeded lock

--- maze_generator_lib/current_maze.cpp
+++ maze_generator_lib/current_maze.cpp
@@ -1,11 +1,8 @@
-#include <boost/thread/locks.hpp>
-
 #include "current_maze.h"
 #include "maze.h"
 
 std::auto_ptr<maze>
 current_maze::current_maze_(new maze (0, 0, location_t(), location_t()));
-mutex current_maze::locker_;
 
 void current_maze::release()
 {
@@ -22,9 +19,4 @@ void current_maze::set(std::auto_ptr<maze> m)
 {
     current_maze_.reset();
     current_maze_ = m;
-}
-
-boost::mutex& current_maze::mutex()
-{
-    return locker_;
 }
